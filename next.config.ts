af1423af92200563c00ca09e5ffa808a39925c0a
DoniwaljiV2 <chetandoniwal404@gmail.com>
2025-12-01 12:10:45 +0530

    Refactor Sanity configuration and update image handling; add baseline-browser-mapping dependency

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,32 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images:{
+//     domains: ['readdy.ai', 'cdn.sanity.io'],
+//   }
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    domains: ['readdy.ai', 'cdn.sanity.io'],
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "readdy.ai",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
